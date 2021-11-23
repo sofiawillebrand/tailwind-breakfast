8b04e1797a2404eaeda0383caddfb5da9ae1b96c
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-11-23 10:32:15 +0100

    update with code from codealong

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 const App = () => {
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div className="flex justify-between">
+      <h1 className="text-black-50">Hello!</h1>
       <h1>World!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
